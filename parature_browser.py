57b51dddf6b355f7bab226d115bd1386886578a3
Jeremy French <jeremy@jeremyfrench.co.uk>
2013-06-24 10:03:26 +0100

    commit code that will get a file before refactoring it away

--- parature_browser.py
+++ parature_browser.py
@@ -1,4 +1,6 @@
 import requests
+import cgi
+import os
 
 class parature_browser(object):
     def __init__(self,config):
@@ -10,13 +12,29 @@ class parature_browser(object):
         payload['email'] = self.config['PARATURE_EMAIL']
         payload['password'] = self.config['PARATURE_PASSWORD']
         #TODO: config for this 
-        x = self.session.post('https://s5-sandbox.parature.com/ics/service/loginSQL.asp', data = payload) 
+        x = self.session.post('https://s5.parature.com/ics/service/loginSQL.asp', data = payload) 
         
     def getPage(self, path):
-        path = 'https://s5-sandbox.parature.com/' + path
+        path = 'https://s5.parature.com' + path
         response = self.session.get(path)
         #TODO: some error checking and thowing. 
         return response.text
+    
+    def saveFile(self, url_path, file_path):
+        path = 'https://s5.parature.com' + url_path
+            
+        response = self.session.get(path, allow_redirects=True, stream=True)
+        _, params = cgi.parse_header(response.headers.get('Content-Disposition', ''))
+        filename = params['filename*'].replace("UTF-8''",'')
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        handle = open(file_path + filename, "wb")
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            handle.write(block)
+            
+        
     
 #Just for debug / Testing   
 if __name__ == "__main__":
@@ -31,5 +49,7 @@ if __name__ == "__main__":
 
     pb = parature_browser(config_vars)   
     
-    print pb.getPage('ics/km/kmFileList.asp?questionID=1') 
     
+    print pb.getPage('/ics/km/kmFileList.asp?questionID=60') 
+    
+    pb.saveFile('/ics/dm/DLRedirect.asp?fileID=37842', '')
